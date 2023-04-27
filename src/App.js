03e92c768cca13502b45345670b46c5b6ca46208
Gloria-Abedi-Gyemfa <gloria.gyemfa@amalitech.com>
2023-04-27 11:00:18 +0000

    learnt how to create, delete and edit during this project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css'
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import ExpenseList from './components/ExpenseList'
 import ExpenseForm from './components/ExpenseForm'
 import Alert from './components/Alert'
 import { v4 as uuidv4 } from 'uuid';
 
-const initialExpenses = [
-  {id: uuidv4(), charge: "rent", amount:1600},
-  {id: uuidv4(), charge: "car", amount:400},
-  {id: uuidv4(), charge: "credit card bill", amount:1200}
-]
+// const initialExpenses = [
+//   {id: uuidv4(), charge: "rent", amount:1600},
+//   {id: uuidv4(), charge: "car", amount:400},
+//   {id: uuidv4(), charge: "credit card bill", amount:1200}
+// ]
+
+
+const initialExpenses = localStorage.getItem('expenses')? JSON.parse(localStorage.getItem('expenses')): []
 
 const App = () => {
   //state values
@@ -22,6 +25,16 @@ const App = () => {
   const [amount,setAmount] = useState('')
   //alert
   const [alert, setAlert] = useState({show:false})
+  //edit
+  const [edit, setEdit] = useState(false)
+  // edit item
+  const [id, setId] = useState(0)
+
+  //useEffect
+  useEffect(()=>{
+    console.log('called use effect')
+    localStorage.setItem('expense', JSON.stringify(expenses))
+  },[expenses])
 
  // functionality
   const handleCharge = e =>{
@@ -42,15 +55,27 @@ const App = () => {
   const handleSubmit = e =>{
     e.preventDefault()
     if(charge !== "" && amount > 0){
-      const singleExpense = {
+      if(edit){
+        let tempExpenses = expenses.map(item=>{
+          return item.id === id ? {...item, charge, amount} :item
+        })
+        setExpenses(tempExpenses);
+        setEdit(false)
+        handleAlert({type:'success', text:"item edited"})
+
+
+      }else{
+        const singleExpense = {
         id: uuidv4(),
         charge: charge,
         amount
       }
       setExpenses([...expenses, singleExpense])
+      handleAlert({type:'success', text:"all items  added"})
+    }
       setAmount("")
       setCharge('')
-      handleAlert({type:'success', text:"all items  added"})
+      
     }else{
       handleAlert({type: 'danger', text:"charge can't be empty and amount value has to be bigger than zero"})
     }
@@ -59,12 +84,20 @@ const App = () => {
   const clearItems =()=>{
     setExpenses([]);
   }
-  const handleDelete = (id)=>{
+  const handleDelete = id=>{
     let tempexpenses = expenses.filter(item=>item.id !== id)
     setExpenses(tempexpenses)
     handleAlert({type:"danger", text:"item deleted"})
   }
-  const handleEdit = (id)=>{}
+  const handleEdit = id=>{
+    let expense = expenses.find((item)=>item.id ===id)
+    let {charge, amount} = expense;
+    setCharge(charge)
+    setAmount(amount)
+    setEdit(true)
+    setId(id)
+    console.log(expense);
+  }
 
 
   return (
@@ -78,7 +111,8 @@ const App = () => {
         amount={amount}
         handleAmount={handleAmount}
         handleCharge={handleCharge}
-        handleSubmit={handleSubmit}/>
+        handleSubmit={handleSubmit}
+        edit={edit}/>
       <ExpenseList 
         expenses ={expenses} 
         handleDelete={handleDelete}
